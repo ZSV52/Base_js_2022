5fa95b54187a47dc541255c1f7f6b8016e525530
ZSV52 <mixgus88@gmail.com>
2022-02-25 22:58:21 +0300

    made homework

---
+++ lesson1/index.js
@@ -0,0 +1,27 @@
+console.log("HELLO JAVA SCRIPT");
+
+const ourPlanetName = "Earth";
+
+console.log(ourPlanetName);
+
+let currentUserTime = "22:54";
+
+console.log(currentUserTime);
+
+let amountArticles = "10";
+
+console.log(amountArticles);
+
+let firstName = "Mikhail";
+
+console.log(firstName);
+
+let surName = "Gusev";
+
+console.log(surName);
+
+const patroNymic = "Aleksandrovich";
+
+console.log(patroNymic);
+
+const isCash = "true";
