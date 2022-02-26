16c8646bd0a38342595981a568b23ec5eb3351e3
ZSV52 <mixgus88@gmail.com>
2022-02-26 09:50:40 +0300

    updated

--- lesson3/index.js
+++ lesson3/index.js
@@ -2,4 +2,17 @@
 
 // Number
 // console.log(1 / 0);
-console.log(typeof NaN);
+
+// console.log(typeof NaN);
+
+// Оператор Typeof показывает тип данных, указанный после Typeof
+
+//String
+
+const myString = "строка";
+
+const anotherString = 'текст: "текст" текст';
+
+const templateString = `шаблонная строка ${anotherString}`;
+
+console.log(templateString);
