95e808ea7e5e7a21e87c2123e5f7906685ea6de5
ZSV52 <mixgus88@gmail.com>
2022-03-11 17:49:55 +0300

    hw_upd

---
+++ lesson4/homework/index.js
@@ -0,0 +1,85 @@
+// Работа с if-else
+
+// 1.
+
+// const a = -3;
+
+// if (a === 0) {
+//   console.log("Верно");
+// } else {
+//   console.log("Неверно");
+// }
+
+// 2.
+
+// const a = -3;
+
+// if (a > 0) {
+//   console.log("Верно");
+// } else {
+//   console.log("Неверно");
+// }
+
+// 3.
+
+// const a = -3;
+
+// if (a < 0) {
+//   console.log("Верно");
+// } else {
+//   console.log("Неверно");
+// }
+
+// 4.
+
+// const a = -3;
+
+// if (a >= 0) {
+//   console.log("Верно");
+// } else {
+//   console.log("Неверно");
+// }
+
+// 5.
+
+// const a = -3;
+
+// if (a <= 0) {
+//   console.log("Верно");
+// } else {
+//   console.log("Неверно");
+// }
+
+// 6.
+
+// const a = -3;
+
+// if (a != 0) {
+//   console.log("Верно");
+// } else {
+//   console.log("Неверно");
+// }
+
+// 7.
+
+// const a = 3;
+
+// if (a === "test") {
+//   console.log("Верно");
+// } else {
+//   console.log("Неверно");
+// }
+
+// 8.
+
+// const a = 3;
+
+// if (a === "1") {
+//   console.log("Верно");
+// } else {
+//   console.log("Неверно");
+// }
+
+// Работа с логическими переменными
+
+// 1.
