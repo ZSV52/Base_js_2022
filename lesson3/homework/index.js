275c1aa4c0ecc5b5e53b132db2218e7da2917426
ZSV52 <mixgus88@gmail.com>
2022-03-17 09:37:32 +0300

    struct_upd

---
+++ lesson3/homework/index.js
@@ -0,0 +1,21 @@
+// "кот" > "код"
+// "2" + 2 * "2"
+// undefined == null
+// undefined != null
+// null == 0
+// 2 > "3"
+// null - false + true
+// 1 / "l"
+// "2" * "3"
+// 4 + 5 + "O"
+// "l" + 4 + 5
+// "4" - 2
+// "4" - "4x"
+// “23” == 23
+// null == false
+// " -4 "/ 0 + 1
+// null + 1
+// undefined + null
+// 1 === “1”
+// “2” > 10
+// NaN == undefined
