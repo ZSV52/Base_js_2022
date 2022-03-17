50e4d7537bfe0e39c73e32f7dbe79ab9b7915452
ZSV52 <mixgus88@gmail.com>
2022-03-17 10:59:52 +0300

    structure_upd

--- lesson3/homework/index.js
+++ lesson3/homework/index.js
@@ -1,21 +1,21 @@
-// "кот" > "код"
-// "2" + 2 * "2"
-// undefined == null
-// undefined != null
-// null == 0
-// 2 > "3"
-// null - false + true
-// 1 / "l"
-// "2" * "3"
-// 4 + 5 + "O"
-// "l" + 4 + 5
-// "4" - 2
-// "4" - "4x"
-// “23” == 23
-// null == false
-// " -4 "/ 0 + 1
-// null + 1
-// undefined + null
-// 1 === “1”
-// “2” > 10
-// NaN == undefined
+// console.log("1)", "кот" > "код"); // true
+// console.log("2)", "2" + 2 * "2"); // 24
+// console.log("3)", undefined == null); // true
+// console.log("4)", undefined != null); // false
+// console.log("5)", null == 0); // false
+// console.log("6)", 2 > "3"); // false
+// console.log("7)", null - false + true); // 1
+// console.log("8)", 1 / "l"); // NaN
+// console.log("9)", "2" * "3"); // 6
+// console.log("10)", 4 + 5 + "O"); // 90
+// console.log("11)", "l" + 4 + 5); // l45
+// console.log("12)", "4" - 2); // 2
+// console.log("13)", "4" - "4x"); // NaN
+// console.log("14)", "23" == 23); // true
+// console.log("15)", null == false); // false
+// console.log("16)", " -4 " / 0 + 1); // -infinity
+// console.log("17)", null + 1); // 1
+// console.log("18)", undefined + null); // NaN
+// console.log("19)", 1 === "1"); // false
+// console.log("20)", "2" > 10); // false
+// console.log("21)", NaN == undefined); // false
